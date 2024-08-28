14d4290d019968041ab5dcf88d696afdc284e2e3
Madie Jones <madelinejones214@gmail.com>
2024-08-27 21:00:03 -0500

    updates to within_usa.py

--- within_usa.py
+++ within_usa.py
@@ -1,3 +1,5 @@
+from shapely.geometry import Polygon, Point
+
 # Continental USA coordinates
 UStop = 49.3457868 # north lat
 USleft = -124.7844079 # west long
@@ -11,10 +13,10 @@ AKright = -140.49 # east long
 AKbottom =  54.20 # south lat
 
 # Hawai'i coordinates
-HAtop = 22.38 # north lat
-HAleft = -160.34 # west long
-HAright = -154.66 # east long
-HAbottom =  18.71 # south lat
+HItop = 22.38 # north lat
+HIleft = -160.34 # west long
+HIright = -154.66 # east long
+HIbottom =  18.71 # south lat
 
 # Puerto Rico & USVI coordinates
 PRtop = 18.60 # north lat
@@ -22,15 +24,36 @@ PRleft = -67.39 # west long
 PRright = -64.29 # east long
 PRbottom =  17.63 # south lat
 
-def check_within_us(lng, lat):
+def coords_to_polygon(left: float, right: float, top: float, bottom: float):
+    """Converts a list of coordinates to a WKT polygon"""
+
+    # Define the coordinates for the polygon (list of tuples)
+    coordinates = [
+        (left, bottom),  # bottom left
+        (right, bottom),   # bottom right
+        (right, top),   # top right
+        (left, top)   # top left
+    ]
+
+    # Return a polygon using the coordinates
+    return = Polygon(coordinates)
+
+
+def check_within_us(lon: float, lat: float) -> bool:
+    """Given a lat/lon, determine whether or not the point is in the USA."""
+
+    point = Point(lon, lat)
     
-    if USbottom <= lat <= UStop and USleft <= lng <= USright:
-        return True
-    elif AKbottom <= lat <= AKtop and AKleft <= lng <= AKright:
-        return True
-    elif HAbottom <= lat <= HAtop and HAleft <= lng <= HAright:
-        return True
-    elif PRbottom <= lat <= PRtop and PRleft <= lng <= PRright:
-        return True
-    else:
-        return None
+    # Check if the point is within the polygon
+    is_inside = polygon.contains(point)
+
+    US = coords_to_polygon(USleft, USright, UStop, USbottom)
+    AK = coords_to_polygon(AKleft, AKright, AKtop, AKbottom)
+    HI = coords_to_polygon(HIleft, HIright, HItop, HIbottom)
+    PR = coords_to_polygon(PRleft, PRright, PRtop, PRbottom)
+
+    for bounds in [US, AK, HI, PR]:
+        if bounds.contains(point):
+            return True
+
+    return False
